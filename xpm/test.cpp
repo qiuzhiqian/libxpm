90e906c34ace04208be1c10c960dd3ec51bc8f95
xml <xiamengliang@gmail.com>
2023-09-13 16:55:36 +0800

    test参数不正确的时候直接返回错误

--- xpm/test.cpp
+++ xpm/test.cpp
@@ -5,6 +5,7 @@
 int main(int argc,char** argv) {
     if(argc != 2) {
         std::cout << argv[0] << " test_xpm_dir" << std::endl;
+        return -1;
     }
     std::vector<std::string> list{
         "logo_1.xpm",
